04f6aa432755f08e5b47bbb3ff898e097b4acd8d
lolos0516 <lolos0516@GMAIL.COM>
2020-04-02 14:31:54 +1300

    Update Exercise_00.R

--- Reactivity/Exercise_00.R
+++ Reactivity/Exercise_00.R
@@ -15,6 +15,14 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # Assignment: Plot the first input$nrows columns of a
   # data frame of your choosing, using head() and plot()
+  output$plot <- renderPlot({ # renderPlot tells shiny the way to update output$plot, rather than what/when to do
+    plot(head(cars, inputs$nrows)) # shiny will decide when/what to do
+    })
+  # option2: anti-solution
+  observe({ # observer will respond to every reactive value in the code
+    df <- head(cars, input$nrows) # will plot even when there was no nrows input
+    output$plot <- renderPlot(plot(df)) 
+    })
 }
 
 shinyApp(ui, server)
